3c080d2bf86c47ce8db7a7877051073892d92956
Nerses <nersamirxanyan@gmail.com>
2017-06-02 14:59:15 +0400

    no message

--- EasyWorld/app/src/main/java/easyworld/gitc/com/easyworld/view/activities/LoginActivity.java
+++ EasyWorld/app/src/main/java/easyworld/gitc/com/easyworld/view/activities/LoginActivity.java
@@ -7,7 +7,6 @@ import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.support.annotation.NonNull;
 import android.support.design.widget.Snackbar;
-import android.support.v7.app.AppCompatActivity;
 import android.app.LoaderManager.LoaderCallbacks;
 
 import android.content.CursorLoader;
@@ -19,11 +18,9 @@ import android.os.AsyncTask;
 import android.os.Build;
 import android.os.Bundle;
 import android.provider.ContactsContract;
-import android.support.v7.widget.Toolbar;
 import android.text.TextUtils;
 import android.view.KeyEvent;
 import android.view.View;
-import android.view.View.OnClickListener;
 import android.view.inputmethod.EditorInfo;
 import android.widget.ArrayAdapter;
 import android.widget.AutoCompleteTextView;
@@ -42,11 +39,7 @@ import static android.Manifest.permission.READ_CONTACTS;
  * A login screen that offers login via email/password.
  */
 
-//public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<Cursor>, OnClickListener {
-
 public class LoginActivity extends BaseActivity implements LoaderCallbacks<Cursor>, View.OnClickListener {
-
-
     /**
      * Id to identity READ_CONTACTS permission request.
      */
